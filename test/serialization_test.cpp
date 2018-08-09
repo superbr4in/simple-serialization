dccf3f6cee7f4c37ac6cb82bcc7c2cba74297ac8
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-08-09 07:47:55 +0200

    Test update

--- test/serialization_test.cpp
+++ test/serialization_test.cpp
@@ -3,14 +3,20 @@
 
 #include "simple-serialization/serialization.h"
 
-static std::vector<uint8_t> extract_buffer(std::ostringstream const& sstream)
+static std::vector<uint8_t> get_buffer(std::ostringstream const& sstream)
 {
     auto const str = sstream.str();
-
     auto const* ptr = reinterpret_cast<uint8_t const*>(str.c_str());
-    auto const size = str.size();
 
-    return std::vector<uint8_t>(ptr, ptr + size);
+    return std::vector<uint8_t>(ptr, ptr + str.size());
+}
+static void put_buffer(std::istringstream& sstream, std::vector<uint8_t> const& buffer)
+{
+    std::string str;
+    for (char const byte : buffer)
+        str += byte;
+
+    sstream.str(str);
 }
 
 static void print_buffer(std::vector<uint8_t> const& buffer)
@@ -34,23 +40,22 @@ static void print_buffer(std::vector<uint8_t> const& buffer)
     std::cout << "| = " << std::dec << buffer.size() << " bytes" << std::endl;
 }
 
-namespace general
+namespace serialization
 {
-    TEST_CASE("test1")
+    TEST_CASE("serialize std::vector<std::optional<unsigned>>")
     {
         std::vector<std::optional<unsigned>> const in = { std::nullopt, 5, 132847, std::nullopt, 0, 42 };
         std::vector<uint8_t> const out = { 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x05, 0x00, 0x00, 0x00, 0x01, 0xEF, 0x06, 0x02, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01, 0x2A, 0x00, 0x00, 0x00 };
 
         std::ostringstream sstream;
-        serialize_container(sstream, in);
+        serialize(sstream, in);
 
-        auto const buffer = extract_buffer(sstream);
+        auto const buffer = get_buffer(sstream);
 
         print_buffer(buffer);
         REQUIRE(buffer == out);
     }
-
-    TEST_CASE("test2")
+    TEST_CASE("serialize std::string")
     {
         std::string const in = "this is a test string!@#$%^&*()_+";
         std::vector<uint8_t> const out = { 0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x61, 0x20, 0x74, 0x65, 0x73, 0x74, 0x20, 0x73, 0x74, 0x72, 0x69, 0x6E, 0x67, 0x21, 0x40, 0x23, 0x24, 0x25, 0x5E, 0x26, 0x2A, 0x28, 0x29, 0x5F, 0x2B, 0x00 };
@@ -58,23 +63,67 @@ namespace general
         std::ostringstream sstream;
         serialize(sstream, in);
 
-        auto const buffer = extract_buffer(sstream);
+        auto const buffer = get_buffer(sstream);
 
         print_buffer(buffer);
         REQUIRE(buffer == out);
     }
-
-    TEST_CASE("test3")
+    TEST_CASE("serialize std::map<int16_t, bool>")
     {
         std::map<int16_t, bool> const in = { { 24, true }, { 16, false }, { -4, false } };
         std::vector<uint8_t> const out = { 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFC, 0xFF, 0x00, 0x10, 0x00, 0x00, 0x18, 0x00, 0x01 };
 
         std::ostringstream sstream;
-        serialize_container(sstream, in);
+        serialize(sstream, in);
 
-        auto const buffer = extract_buffer(sstream);
+        auto const buffer = get_buffer(sstream);
 
         print_buffer(buffer);
         REQUIRE(buffer == out);
+    }
+}
+namespace deserialization
+{
+    TEST_CASE("deserialize std::vector<std::optional<unsigned>>")
+    {
+        std::vector<uint8_t> const in = { 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x05, 0x00, 0x00, 0x00, 0x01, 0xEF, 0x06, 0x02, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01, 0x2A, 0x00, 0x00, 0x00 };
+        std::vector<std::optional<unsigned>> const out = { std::nullopt, 5, 132847, std::nullopt, 0, 42 };
+
+        std::istringstream sstream;
+        put_buffer(sstream, in);
+
+        std::optional<int> x;
+        *x = 7;
+
+        std::vector<std::optional<unsigned>> container;
+        deserialize(sstream, container);
+
+        REQUIRE(container == out);
+    }
+    TEST_CASE("deserialize std::string")
+    {
+        std::vector<uint8_t> const in = { 0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x61, 0x20, 0x74, 0x65, 0x73, 0x74, 0x20, 0x73, 0x74, 0x72, 0x69, 0x6E, 0x67, 0x21, 0x40, 0x23, 0x24, 0x25, 0x5E, 0x26, 0x2A, 0x28, 0x29, 0x5F, 0x2B, 0x00 };
+        std::string const out = "this is a test string!@#$%^&*()_+";
+
+        std::istringstream sstream;
+        put_buffer(sstream, in);
+
+        std::string string;
+        deserialize(sstream, string);
+
+        REQUIRE(string == out);
+    }
+    TEST_CASE("deserialize std::map<int16_t, bool>")
+    {
+        std::vector<uint8_t> const in = { 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFC, 0xFF, 0x00, 0x10, 0x00, 0x00, 0x18, 0x00, 0x01 };
+        std::map<int16_t, bool> const out = { { 24, true },{ 16, false },{ -4, false } };
+
+        std::istringstream sstream;
+        put_buffer(sstream, in);
+
+        std::map<int16_t, bool> container;
+        deserialize(sstream, container);
+
+        REQUIRE(container == out);
     }
 }
