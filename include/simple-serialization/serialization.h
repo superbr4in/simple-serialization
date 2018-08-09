e71f2354fe6535eb1199e9578d095176abb4cf43
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-08-09 07:47:17 +0200

    Added deserialization methods and replaced generic container functionality.

--- include/simple-serialization/serialization.h
+++ include/simple-serialization/serialization.h
@@ -4,23 +4,52 @@
 #include <ostream>
 #include <string>
 
-void serialize(std::ostream& stream, std::string const& value);
+void serialize(std::ostream& stream, std::string const& string);
+void deserialize(std::istream& stream, std::string& string);
 
 template <typename T>
 void serialize(std::ostream& stream, T const& value);
 template <typename T>
-void serialize(std::ostream& stream, std::optional<T> optional);
+void deserialize(std::istream& stream, T& value);
+
+template <typename T>
+void serialize(std::ostream& stream, std::optional<T> const& optional);
+template <typename T>
+void deserialize(std::istream& stream, std::optional<T>& optional);
+
+template <typename T>
+void serialize(std::ostream& stream, std::vector<T> const& vector);
+template <typename T>
+void deserialize(std::istream& stream, std::vector<T>& vector);
 
 template <typename T1, typename T2>
 void serialize(std::ostream& stream, std::pair<T1, T2> const& pair);
+template <typename T1, typename T2>
+void deserialize(std::istream& stream, std::pair<T1, T2>& pair);
 
-template <typename Container>
-void serialize_container(std::ostream& stream, Container container);
+template <typename T1, typename T2>
+void serialize(std::ostream& stream, std::map<T1, T2> const& map);
+template <typename T1, typename T2>
+void deserialize(std::istream& stream, std::map<T1, T2>& map);
 
-inline void serialize(std::ostream& stream, std::string const& value)
+inline void serialize(std::ostream& stream, std::string const& string)
 {
-    stream.write(value.c_str(), value.size());
+    stream.write(string.c_str(), string.size());
     stream.put('\0');
+}
+inline void deserialize(std::istream& stream, std::string& string)
+{
+    std::string result;
+    while (true)
+    {
+        auto const c = stream.get();
+        if (c == '\0')
+            break;
+
+        result += c;
+    }
+
+    string = result;
 }
 
 template <typename T>
@@ -29,13 +58,53 @@ void serialize(std::ostream& stream, T const& value)
     stream.write(reinterpret_cast<char const*>(&value), sizeof value);
 }
 template <typename T>
-void serialize(std::ostream& stream, std::optional<T> optional)
+void deserialize(std::istream& stream, T& value)
+{
+    stream.read(reinterpret_cast<char*>(&value), sizeof value);
+}
+
+template <typename T>
+void serialize(std::ostream& stream, std::optional<T> const& optional)
 {
     auto const has_value = optional.has_value();
     
     stream.put(has_value);
     if (has_value)
         serialize(stream, *optional);
+}
+template <typename T>
+void deserialize(std::istream& stream, std::optional<T>& optional)
+{
+    if (stream.get())
+    {
+        T value;
+        deserialize(stream, value);
+
+        optional = value;
+    }
+    else optional = std::nullopt;
+}
+
+template <typename T>
+void serialize(std::ostream& stream, std::vector<T> const& vector)
+{
+    serialize(stream, static_cast<uint64_t>(vector.size()));
+    for (auto const& elem : vector)
+        serialize(stream, elem);
+}
+template <typename T>
+void deserialize(std::istream& stream, std::vector<T>& vector)
+{
+    uint64_t size;
+    deserialize(stream, size);
+
+    for (uint64_t i = 0; i < size; ++i)
+    {
+        T elem;
+        deserialize(stream, elem);
+
+        vector.push_back(elem);
+    }
 }
 
 template <typename T1, typename T2>
@@ -44,11 +113,31 @@ void serialize(std::ostream& stream, std::pair<T1, T2> const& pair)
     serialize(stream, pair.first);
     serialize(stream, pair.second);
 }
-
-template <typename Container>
-void serialize_container(std::ostream& stream, Container container)
+template <typename T1, typename T2>
+void deserialize(std::istream& stream, std::pair<T1, T2>& pair)
 {
-    serialize(stream, static_cast<uint64_t>(container.size()));
-    for (auto const& elem : container)
+    deserialize(stream, pair.first);
+    deserialize(stream, pair.second);
+}
+
+template <typename T1, typename T2>
+void serialize(std::ostream& stream, std::map<T1, T2> const& map)
+{
+    serialize(stream, static_cast<uint64_t>(map.size()));
+    for (auto const& elem : map)
         serialize(stream, elem);
+}
+template <typename T1, typename T2>
+void deserialize(std::istream& stream, std::map<T1, T2>& map)
+{
+    uint64_t size;
+    deserialize(stream, size);
+
+    for (uint64_t i = 0; i < size; ++i)
+    {
+        std::pair<T1, T2> elem;
+        deserialize(stream, elem);
+
+        map.insert(elem);
+    }
 }
