dc41a2b96dc30144058cb9192d5bf46bb32a53a3
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-12-21 12:47:40 +0100

    Introduced .hpp extension.

--- include/stored-bits/serialization.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "serialization_generic.h"
-#include "serialization_map.h"
-#include "serialization_optional.h"
-#include "serialization_pair.h"
-#include "serialization_string.h"
-#include "serialization_vector.h"
---
+++ include/stored-bits/serialization.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "serialization_generic.hpp"
+#include "serialization_map.hpp"
+#include "serialization_optional.hpp"
+#include "serialization_pair.hpp"
+#include "serialization_string.hpp"
+#include "serialization_vector.hpp"
